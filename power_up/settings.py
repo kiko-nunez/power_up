ce3c33b7146ab9060624a0a53228d8285a6ee875
kiko-nunez <117489647+kiko-nunez@users.noreply.github.com>
2023-02-09 16:48:18 -0700

    adds static root

--- power_up/settings.py
+++ power_up/settings.py
@@ -124,6 +124,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'main_app', 'static')
+
 STATIC_URL = 'static/'
 
 LOGIN_REDIRECT_URL = '/'

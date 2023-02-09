f9de76b725e3a6ee0c7acaa087eb6a59e1568689
kiko-nunez <117489647+kiko-nunez@users.noreply.github.com>
2023-02-09 16:50:27 -0700

    moves django import to bottom

--- power_up/settings.py
+++ power_up/settings.py
@@ -12,10 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
 from dotenv import load_dotenv
 load_dotenv()
-django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -136,3 +134,7 @@ LOGOUT_REDIRECT_URL = '/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+import django_heroku
+django_heroku.settings(locals())
